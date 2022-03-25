d6993615e648e54e4bf1e3ecf70266c3b45ca798
Dominic <dadocimo@gmail.com>
2022-03-25 10:01:45 -0400

    Create config template

---
+++ _config.py
@@ -0,0 +1,19 @@
+# Your Windows user profile name
+windows_profile_name = "JohnSmith"
+
+# Find this via chrome://version
+chrome_profile_name = "Profile 1"
+
+# Last name only
+teachers = ["...", "...", "...", "...", "...", "...", "..."]
+
+# First as name[0], last as name[1]
+name = ["John", "Smith"]
+
+message = "I am going to the restroom"
+
+# Search 'entry.' while using inspect element to find which entries are available
+entries = ["entry.1234", "...", "...", "...", "...", "...", "..."]
+
+# The ID of the actual form (e.g. https://docs.google.com/forms/d/e/.../)
+form_id = "1234abcd"
